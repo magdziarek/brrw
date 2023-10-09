d8f2b679df4c3101c2c23f91f2f870d6094fa2f2
Aleksandra Magdziarek <aleksandra.magdziarek@trialbee.com>
2023-10-09 08:22:57 +0200

    feat: add orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ItemModule } from './item/item.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, ItemModule],
+  imports: [
+    UserModule,
+    ItemModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      autoLoadEntities: true,
+      synchronize: true, // change for prod
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
